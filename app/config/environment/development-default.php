bb7480913c5d6149eef00cd726bacd67dd6a4e1a
Luis Medrano <lmedrano@multisistemas.com.sv>
2017-03-17 11:43:46 -0600

    minor changes

---
+++ app/config/environment/development-default.php
@@ -0,0 +1,50 @@
+<?php
+
+return [
+    'base_path' => '/',
+
+    'database'  => [
+        'adapter'  => 'Mysql',
+        'host'     => 'localhost',
+        'username' => 'root',
+        'password' => 'toor',
+        'dbname'   => 'yona',
+        'charset'  => 'utf8',
+    ],
+
+    'memcache'  => [
+        'host' => 'localhost',
+        'port' => 3306,
+    ],
+
+    'memcached'  => [
+        'host' => 'localhost',
+        'port' => 3306,
+    ],
+
+    'cache'     => 'file', // memcache, memcached
+
+    
+    // Opauth configuration
+    'opauth' => [
+        'path' => '/dashboard/login/loginOpauth/',
+        'callback_url' => 'http://your-server.com/dashboard/login/success',
+        'security_salt' => 'LHFm11lYf3Fyw5W10a44aa5x4W1KsVrieQCnpBzzpTBMA5vJidQKDo8pMJbmw22A1C8v',
+        'debug' =>true,
+        'Strategy' => [
+            'Google' => [
+                'client_id' => '708944901374-55hfdptar6ej5n01nkg7adsmd13dtr6p.apps.googleusercontent.com',
+                'client_secret' => 'pMX7H5Lr_oB7pC-oooKtgIVL',
+                'redectUrl' =>'http://your-server.com/dashboard/login/loginOpauth/google/oauth2callback',
+            ],
+        ],
+    ],
+
+    'swift' => [
+        'server'    => 'a2ss35.a2hosting.com',
+        'port'      => 25,
+        'username'  => 'no-reply@multisistemax.com',
+        'password'  => 'VKPSkevoC2+h',
+    ],
+
+];
